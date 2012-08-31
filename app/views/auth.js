cfc0d8be3c041b3326b18eed764f3307a94abf4f
Sergey Chernov <uriiniix@gmail.com>
2012-09-01 03:08:25 +0400

    Added authentication section to application menu, and tab to objects view

--- app/views/auth.js
+++ app/views/auth.js
@@ -1,9 +1,9 @@
 define([
 
     "backbone",
-    "plugins/backbone.marionette"
+    "plugins/backbone.marionette",
 
-
+    "slickgrid"
 
 ], function (Backbone, Marionette, SlickGrid) {
 
@@ -16,6 +16,41 @@ define([
         },
 
         onShow:function () {
+            $('#auth_tabs a').click(function (e) {
+                e.preventDefault();
+                $(this).tab('show');
+            });
+
+            var users = [
+                {
+                    user_name: "agent1",
+                    password: "s3creT",
+                    access_token: "1122333312lckckkc"
+                },
+                {
+                    user_name: "agent2",
+                    password: "s3creT",
+                    access_token: "1122333312lckckkc"
+                },
+                {
+                    user_name: "agent3",
+                    password: "s3creT",
+                    access_token: "1122333312lckckkc"
+                }
+            ];
+
+            var user_columns = [
+                {id:"user_name", name:"Name", field:"user_name"},
+                {id:"password", name:"Password", field:"password"},
+                {id:"access_token", name:"Token", field:"access_token"}
+            ];
+
+            var options = {
+                enableCellNavigation:true,
+                enableColumnReorder:false
+            };
+
+            var users_grid = new Slick.Grid("#users", users, user_columns, options);
         },
 
         events:{
